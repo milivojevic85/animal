ef3289867a5cae605dd5023c6967f56582d29aed
milivojevic85 <misagrf@yahoo.com>
2020-01-20 21:58:23 +0100

    protected =__CLASS__ and __get() method added

--- dog.php
+++ dog.php
@@ -2,12 +2,21 @@
 require('animal.php');
 Class Dog extends Animal
 {
+	protected $type=__CLASS__;
+	public function __get($property) {
+		if (property_exists($this, $property)) {
+			return $this->$property."<br>";
+		} else {
+			return $property." does not exist<br>";
+		}
+	}
 	public function run() {
 		return $this->name." likes to run<br>";
 	}
 }
 
 $dog = new Dog("Scooby");
+echo $dog->type;
 echo $dog->greet();
 echo $dog->run();
 
